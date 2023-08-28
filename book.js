092c9e5f6c487a2a1abc04ae085de9b3ee0bc636
Shahzar-Ali <shahzzarralii@gmail.com>
2023-08-28 22:01:07 +0500

    first commit

---
+++ book.js
@@ -0,0 +1,22 @@
+const Orders = [
+    {
+        productName: 'Cloud Computing',
+        productNumber: '92873',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Machine Learning',
+        productNumber: '28397',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Web Development',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
+
+
